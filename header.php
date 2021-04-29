5f6a9c08af0be77f7fbc11fa9a632eb54bd584f9
mikejeter <mjeter45@yahoo.com>
2021-04-29 19:03:37 -0400

    Created wp-nav-menu added container and styling for menu

--- header.php
+++ header.php
@@ -12,17 +12,19 @@
 <body>
 
 <header>
+    
+<div class="container">
+    <?php 
+    wp_nav_menu(
 
- <?php 
- wp_nav_menu(
-
-    array(
-        'theme_location' => 'top-menu',
-    )
- );
- 
- ?>
-
+        array(
+            'theme_location' => 'top-menu',
+            'menu_class' => 'top-bar',
+        )
+    );
+    
+    ?>
+</div>
 
 
 </header>
