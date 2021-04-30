ad3a29ee8b024540836ef6bb7b70d96437f4f053
mikejeter <mjeter45@yahoo.com>
2021-04-30 12:30:00 -0400

    Added css styling to menu and pages

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-
+<section class="page-wrap">
 <div class="container">
 
     <h1><?php the_title(); ?></h1>
@@ -7,6 +7,7 @@
     <?php get_template_part('includes/section', 'content');?>
 
 </div>
+</section>
 
 
 
--- template-contactus.php
+++ template-contactus.php
@@ -5,7 +5,7 @@ Template Name: Contact Us
 ?>
 
 <?php get_header(); ?>
-
+<section class="page-wrap">
 <div class="container">
 
     <h1><?php the_title(); ?></h1>
@@ -30,6 +30,7 @@ Template Name: Contact Us
     
 
 </div>
+</section>
 
 
 
